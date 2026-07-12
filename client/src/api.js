25a62e5f8b85e5c402e63855167048f111194108
Himanshu <himanshutiwari03555@gmail.com>
2026-07-12 10:03:39 +0530

    update api url

--- client/src/api.js
+++ client/src/api.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const API = axios.create({
-  baseURL: "http://localhost:5000/api",
+  baseURL: "https://taskflow-server-beot.onrender.com/api",
 });
 
 export default API;
